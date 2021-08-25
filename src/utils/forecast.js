2c6b967e75683294740a1b838a5612f99373a74f
parva-jain <parvajainpjjp@gmail.com>
2021-08-25 19:34:53 +0530

    Added more information about weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (lat, long, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=e8f90223f7871aadf2e8ab213e314d85&query=' + long.toString() + ',' + lat.toString() + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=e8f90223f7871aadf2e8ab213e314d85&query=' + long.toString() + ',' + lat.toString() + '&units=m'
 
     request({url, json:true}, (error, {body} = {}) => {
         if (error){
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
             callback('Invalid geolocation', undefined)
         }
         else {
-            callback(undefined, "It is currently "+ body.current.temperature +" fahrenheit out. It feels like " + body.current.feelslike + " fahrenheit out. The sky is " + body.current.weather_descriptions[0] + " here.")
+            callback(undefined, "It is currently "+ body.current.temperature +" °C. It feels like " + body.current.feelslike + " °C out. The sky is " + body.current.weather_descriptions[0] + " here. Humidity is " + body.current.temperature + "% and wind direction is " + body.current.wind_dir + ".")
         }
 
     })
